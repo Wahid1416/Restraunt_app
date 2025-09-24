005731516bc7199fc11b79cabe2e4382fca11149
Wahid1416 <mdisharaf@gmail.com>
2025-09-25 01:02:13 +0530

    complete final

--- app.py
+++ app.py
@@ -1350,4 +1350,4 @@ def kitchen():
 
 # -------------------- Run App --------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8000,debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
